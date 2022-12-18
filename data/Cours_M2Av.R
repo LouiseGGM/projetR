4dfb7c93bc8a26d270acf4bdcb44e438d72726f4
Guilbaud Louise <louise.guilbaud448@gmail.com>
2022-12-18 14:00:08 +0100

    dossier data ajouté dans le projet. fichier Rmardown, début importation des données

---
+++ data/Cours_M2Av.R
@@ -0,0 +1,6 @@
+#Packages 
+library(tidyverse)
+library(questionr)
+
+coco1 <- read.csv2('C:/Users/louis/Documents/Master/M2/R Avancé/data/cdsp_coco1_2020/donnees_coco1/csv_coco1/fr.cdsp.ddi.elipss.202004coco1.csv')
+
