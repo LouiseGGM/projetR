cdbad941435d6950daf0af54fa2b41eb83c95f42
Guilbaud Louise <louise.guilbaud448@gmail.com>
2022-12-23 01:10:10 +0100

    maj

---
+++ explo_hyp1.R
@@ -0,0 +1,26 @@
+#exploration hypothèse une : 
+#La technologie a permis le maintien d’un certain lien social, 
+#mais son visage reste différentiel selon l’âge. → bon maintien jusqu’à 40 ans.
+
+# coco1_q28 --> nb d'ordi /!\ Score d'équipement numérique
+freq(coco$coco1_q28)
+#La majorité des gens ont entre 2 et 3 ordis (env 68%)
+freq(coco$coco2_q28) #ne fonctionne pas, vérifier le num de var
+freq(coco$coco3_q28) #ne fonctionne pas, vérifier le num de var
+
+# coco1_q29 --> nb de smartphone/tablette /!\ Score d'équipement numérique
+freq(coco$coco1_q29)
+#53 % des gens ont entre 3 et 4 smartphones et/ ou tablettes
+freq(coco$coco2_q29) #ne fonctionne pas, vérifier le num de var
+freq(coco$coco3_q29) #ne fonctionne pas, vérifier le num de var
+
+
+# coco1_q31 --> bonne connexion internet /!\ Score d'équipement numérique
+# coco1_q47 --> fréquences des sorties
+# coco1_q52_1 --> fréquence des rencontres avec des amis dans la vie réelle /!\ combiner famille et amis 
+# coco1_q52_2 --> fréquence des rencontres avec des amis virtuellement
+# coco1_q59_7 --> temps consacré à parler au telephone
+# coco1_q59_8 --> temps consacré à utiliser les réseau sociaux
+# coco1_q61_7 --> temps consacré par rappport à d'habitude à parler au telephone
+# coco1_q61_8 --> temps consacré par rappport à d'habitude à utiliser les réseau sociaux
+# eayy_A2A_rec --> Age quinquennal
