57769229cb5a141f83c455849e2ab13668ac62c0
Guilbaud Louise <louise.guilbaud448@gmail.com>
2022-12-24 21:21:46 +0100

    maj git

--- explo_hyp1.R
+++ explo_hyp1.R
@@ -1,26 +1,88 @@
 #exploration hypothèse une : 
 #La technologie a permis le maintien d’un certain lien social, 
 #mais son visage reste différentiel selon l’âge. → bon maintien jusqu’à 40 ans.
+library(tidyselect)
+library(dplyr)
+library(tidyverse)
+library(questionr)
+
+#Nombre de personnes vivant dans le logement 
+freq(coco$coco1_q23)
+coco$coco1_q23[coco$coco1_q23 ==2.6] <- 3 #On transforme le 2,6 en 3, surtout que ne concerne q'un enquêté.e
+#Peu de ménages comptent plus de 4 individus
+#La plus grande part des foyers sont des ménages de 2 personnes : couple ou parent solo+enf
+
+# eayy_A2A_rec --> Age quinquennal
+freq(coco$eayy_A2A_rec)
+coco$Age_rec <- fct_collapse(factor(coco$eayy_A2A_rec),"-24"="4","25-29"="5",
+                             "30-34"="6","35-39"="7", "40-44"="8", "45-49"="9", "50-54"="10",
+                             "55-59"="11", "60-64"="12", "65-79"="13", "+70"="14")
+
 
 # coco1_q28 --> nb d'ordi /!\ Score d'équipement numérique
 freq(coco$coco1_q28)
 #La majorité des gens ont entre 2 et 3 ordis (env 68%)
-freq(coco$coco2_q28) #ne fonctionne pas, vérifier le num de var
-freq(coco$coco3_q28) #ne fonctionne pas, vérifier le num de var
+
+#tris croisés nb ordi et nb de gens par foyer
+addmargins(table(coco$coco1_q23, coco$coco1_q28))
+#En ligne le nb de pers / foyer et en col le nb d'ordi
+#Parmi les 175 pers vivant seule, 119 ont 2 ordinateurs
+#les pers vivant à 2 ont entre 2 et 3 ordi (respectivement 144,139)
+#le nombre de personnes vivant dans le foyer augmentant, le nb d'ordi stagne autour de 3, 
+    # logique si on pense aux familles 
+
+rtable(table(coco$eayy_A2A_rec,coco$coco1_q28))
+
 
 # coco1_q29 --> nb de smartphone/tablette /!\ Score d'équipement numérique
 freq(coco$coco1_q29)
+median(coco$coco1_q29) #mediane : 4
+mean(coco$coco1_q29) #Moyenne de 51 ???
 #53 % des gens ont entre 3 et 4 smartphones et/ ou tablettes
-freq(coco$coco2_q29) #ne fonctionne pas, vérifier le num de var
-freq(coco$coco3_q29) #ne fonctionne pas, vérifier le num de var
+
+#tris croisés nb smartphones/tab et nb de gens par foyer
+addmargins(table(coco$coco1_q23, coco$coco1_q29))
+#En ligne le nb de pers / foyer et en col le nb d'ordi
+#La majorité des consommateurs de téléphone / tablette sont les foyers de 2 pers
+#Ils en ont entre 3 et 4 soit un smartphone chacun + 1 tablette pour 2 ou une chacun
+#Le score d'équipement numérique est plus sensible à l'augmentation du nombre de personne dans le foyer.
+#Les valeurs sont plus répartis que dans le cas des ordinateurs.
+
+addmargins(table(coco$eayy_A2A_rec,coco$coco1_q29))
+#Les plus de 70 ans sont bien équipés en smartphones et tablettes (2,3,4), ce sont eux qui répondent le plus
+#Les 35-60 sont les plus équipés en nombre par foyers
 
 
 # coco1_q31 --> bonne connexion internet /!\ Score d'équipement numérique
-# coco1_q47 --> fréquences des sorties
+freq(coco$coco1_q31)
+#Beaucoup de NR : 282 6666 soit 33.3% de NR
+
+# coco1_q47 --> fréquences des sorties au cours des 2 dernières semaines
+freq(coco$coco1_q47)
+freq(coco$coco2_q47)
+freq(coco$coco3_q47)
+
+addmargins(table(coco$coco1_q23,coco$coco1_q29))
+addmargins(table(coco$eayy_A2A_rec,coco$coco1_q29))
+
+
+#repartition proche entre avant et après
+#fréquence de visite des ami.e.s
 # coco1_q52_1 --> fréquence des rencontres avec des amis dans la vie réelle /!\ combiner famille et amis 
+freq(coco$coco1_q47)
+
 # coco1_q52_2 --> fréquence des rencontres avec des amis virtuellement
+freq(coco$coco1_q47)
+
 # coco1_q59_7 --> temps consacré à parler au telephone
+freq(coco$coco1_q47)
+
 # coco1_q59_8 --> temps consacré à utiliser les réseau sociaux
+freq(coco$coco1_q47)
+
 # coco1_q61_7 --> temps consacré par rappport à d'habitude à parler au telephone
+freq(coco$coco1_q47)
+
 # coco1_q61_8 --> temps consacré par rappport à d'habitude à utiliser les réseau sociaux
-# eayy_A2A_rec --> Age quinquennal
+freq(coco$coco1_q47)
+
--- recod.R
+++ recod.R
@@ -1,110 +1,147 @@
-#RECODAGE P1
+library(tidyverse)
+library(questionr)
 
-# solitude
-coco$coco1_q21 <- fct_collapse(factor(coco$coco1_q21),"Très seul"="1","Plutôt seul"="2",
-                               "Plutôt entouré"="3","Très entouré"="4")
-freq(coco$coco1_q21)
-
-# coco1_q37 --> situation pro pendant le confinementsolitude
-
-coco$coco1_q37[coco$coco1_q37 %in% c("1","2","3")]<-"Congés maladie parental"
-coco$coco1_q37[coco$coco1_q37 %in% c("4","5")]<-"Chômage"               
-
-coco$coco1_q23[coco$coco1_q23 ==2.6] <- 3
-
-coco$coco1_q28[coco$coco1_q28 %in% c("1","2","3")]<-"Congés maladie parental"
-table(coco$coco1_q37)
-freq(coco$coco1_q23) #recodage n'a pas fonctionné
-
-# coco1_q28 --> nb d'ordi /!\ Score d'éqyupement numérique
-# coco1_q29 --> nb de smartphone/tablette /!\ Score d'éqyupement numérique
-# coco1_q31 --> bonne connexion internet /!\ Score d'éqyupement numérique
+#table femmes uniquement ----
+cocofemme <- coco %>% filter(eayy_A1 == "2")
+freq(cocofemme$coco1_q21)
 
 
-#nb d'ordi par personne
-class(coco$coco1_q28) #class character
-coco$coco1_q28 <- as.factor(coco$coco1_q28) #devient un factor
-freq(coco$coco1_q28) #modalités 4,5,6, 9999 + NA
+#tris croisés pondérés ----
+#Solitude ----
+#solitude/enfants?  (1 oui 2 non)
 
-coco$coco1_q28_rap <- (coco$coco1_q28/coco$coco1_q23)
-#  Warning : ‘/’ n'est pas pertinent pour des variables facteurs
+wtd.table(coco$eayy_C8, coco$coco1_q21, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
 
-freq(coco$coco1_q28_rap) #847 NA
-
-#en dessous de 1 ordi pour trois --> 0
-#moins de 1 --> 1
-#1 (inclu) --> 2
-
-#nb de smartphone par personne
-coco$coco1_q29_rap <- (coco$coco1_q29/coco$coco1_q23)
-freq(coco$coco1_q31) #mod 1, 2 et 6666
-
-#internet --> on peu pas parce que c'est une question filtre
-# coco$coco1_q31[coco$coco1_q31 %in% c(2)]<-0
-# 
-# coco$coco1_score_num[is.na(coco$coco1_q28) | is.na(coco$coco1_q29)]<-NA
-# freq(coco$coco1_score_num)
-
-#reçu de l'aide
-class(coco$coco1_q54) #NULL
-
-coco$coco1_q54 [coco$coco1_q54_1==1 | coco$coco1_q54_2==1 | coco$coco1_q54_3==1 |
-                  coco$coco1_q54_4==1 | coco$coco1_q54_5==1 | coco$coco1_q54_6==1 |
-                  coco$coco1_q54_7==1 ] <- "OUI"
-freq(coco$coco1_q54)
-
-coco$coco1_q54 [coco$coco1_q54_8==1] <- "NON"
-coco$coco1_q54[is.na(coco$coco1_q54) ]<-"NON"
-
-#porté de l'aide
-coco$coco1_q54_bis [coco$coco1_q54_bis_1==1 | coco$coco1_q54_bis_2==1 | coco$coco1_q54_bis_3==1 |
-                      coco$coco1_q54_bis_4==1 | coco$coco1_q54_bis_5==1 | coco$coco1_q54_bis_6==1 |
-                      coco$coco1_q54_bis_7==1 ] <- "OUI"
-
-coco$coco1_q54_bis [coco$coco1_q54_bis_8==1] <- "NON"
-coco$coco1_q54_bis[is.na(coco$coco1_q54_bis)]<-"NON"
-freq(coco$coco1_q54_bis)
-class(coco$coco1_q54_bis) #NULL
+wtd.table(cocofemme$eayy_C8, cocofemme$coco1_q21, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
 
 
-# coco1_q52_1 --> fréquence des rencontres avec des amis dans la vie réelle /!\ combiner famille et amis 
-# coco1_q52_2 --> fréquence des rencontres avec des amis virtuellement
-# coco1_q52_3 --> famille IRL
-# coco1_q52_4 --> famille IVL
-# coco1_q53_1 --> fréquence contact famille à l'étranger
-# coco1_q53_2 --> amis à l'étranger
-# coco1_q53_3 --> collègues à l'étranger
+#solitude/nb enfant
 
-class(coco$coco1_q52_rel) #NULL
-coco$coco1_q52_rel <- as.factor()
-coco$coco1_q52_rel <- ifelse(coco$coco1_q52_1 %in% c("1","2") | coco$coco1_q52_3 %in% c("1","2"), 
-                             "Beaucoup de contact IRL", coco$coco1_q52_rel)
-coco$coco1_q52_rel <- ifelse(coco$coco1_q52_2 %in% c("1","2") | coco$coco1_q52_4 %in% c("1","2"), 
-                             "Beaucoup de contact IVL", coco$coco1_q52_rel)
-coco$coco1_q52_rel <- ifelse(coco$coco1_q53_1 %in% c("1","2") | coco$coco1_q53_2 %in% c("1","2")| 
-                               coco$coco1_q53_3 %in% c("1","2"), "Beaucoup de contact à l'étranger", 
-                             coco$coco1_q52_rel)
+wtd.table(coco$eayy_C8A_rec, coco$coco1_q21, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
 
-coco$coco1_q52_rel <- ifelse(coco$coco1_q52_1 %in% c("3","4") | coco$coco1_q52_3 %in% c("3","4"), 
-                             "un peu de contact IRL", 
-                             coco$coco1_q52_rel)
-coco$coco1_q52_rel <- ifelse(coco$coco1_q52_2 %in% c("3","4") | coco$coco1_q52_4 %in% c("3","4"), 
-                             "un peu de contact IVL", 
-                             coco$coco1_q52_rel)
-coco$coco1_q52_rel <- ifelse(coco$coco1_q53_1 %in% c("3","4") | coco$coco1_q53_2 %in% c("3","4")| 
-                               coco$coco1_q53_3 %in% c("3","4"), "un peu de contact à l'étranger", 
-                             coco$coco1_q52_rel)
+wtd.table(cocofemme$eayy_C8A_rec, cocofemme$coco1_q21, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+#solitude/nb enfant cohabitants
+
+wtd.table(coco$eayy_C8B_rec, coco$coco1_q21, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+wtd.table(cocofemme$eayy_C8B_rec, cocofemme$coco1_q21, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+#Fréquence des sorties ----
+# coco1_q47 --> fréquences des sorties 1 Jamais/2 Une fois par semaine ou -/
+#3 Une fois tous les 2-3 jours/4 Tous les jours ou presque
 
 
-coco$coco1_q52_rel <- ifelse(coco$coco1_q52_1 == 5 | coco$coco1_q52_3 == 5, 
-                             "pas de contact", 
-                             coco$coco1_q52_rel)
-coco$coco1_q52_rel <- ifelse(coco$coco1_q52_2 == 5 | coco$coco1_q52_4 == 5, 
-                             "pas de contact", 
-                             coco$coco1_q52_rel)
-coco$coco1_q52_rel <- ifelse(coco$coco1_q53_1 == 5 | coco$coco1_q53_2 == 5| 
-                               coco$coco1_q53_3 == 5, "pas de contact", 
-                             coco$coco1_q52_rel)
+#Sortie/enfants?  (1 oui 2 non)
 
-coco$coco1_q52_rel[is.na(coco$coco1_q52_rel)]<-"pas de contact"
-freq(coco$coco1_q52_rel)
+wtd.table(coco$eayy_C8, coco$coco1_q47, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+wtd.table(cocofemme$eayy_C8, cocofemme$coco1_q47, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+#sortie/nb enfant
+
+wtd.table(coco$eayy_C8A_rec, coco$coco1_q47, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+wtd.table(cocofemme$eayy_C8A_rec, cocofemme$coco1_q47, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+#sortie/nb enfant cohabitants
+
+wtd.table(coco$eayy_C8B_rec, coco$coco1_q47, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+wtd.table(cocofemme$eayy_C8B_rec, cocofemme$coco1_q47, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+# Stress lié au confinement ----
+# coco1_q64 --> stress lié au confinement (échelle)
+
+wtd.table(coco$eayy_A1, coco$coco1_q64, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+#Stress/enfants?  (1 oui 2 non)
+
+wtd.table(coco$eayy_C8, coco$coco1_q64, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+wtd.table(cocofemme$eayy_C8, cocofemme$coco1_q64, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+#stress/nb enfant
+#faire un bockspot avec les don pondérées
+
+wtd.table(coco$eayy_C8A_rec, coco$coco1_q64, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+wtd.table(cocofemme$eayy_C8A_rec, cocofemme$coco1_q64, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+#stress/nb enfant cohabitants
+
+wtd.table(coco$eayy_C8B_rec, coco$coco1_q64, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+wtd.table(cocofemme$eayy_C8B_rec, cocofemme$coco1_q64, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+#Attentes sur les relations sociales ----
+# coco1_q83_6 --> attente que le confinement affecte ses relations sociales
+#1 pas du tout/un peu/assez/bcp
+
+#Relations sociales/enfants?  (1 oui 2 non)
+
+wtd.table(coco$eayy_C8, coco$coco1_q83_6, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+wtd.table(cocofemme$eayy_C8, cocofemme$coco1_q83_6, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+#Relations sociales/nb enfant
+
+wtd.table(coco$eayy_C8A_rec, coco$coco1_q83_6, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+wtd.table(cocofemme$eayy_C8A_rec, cocofemme$coco1_q83_6, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+#Relations sociales/nb enfant cohabitants 
+
+wtd.table(coco$eayy_C8B_rec, coco$coco1_q83_6, 
+          weights = coco$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
+
+wtd.table(cocofemme$eayy_C8B_rec, cocofemme$coco1_q83_6, 
+          weights = cocofemme$coco1_POIDS, useNA = "ifany") %>% 
+  cprop(digits=1)
